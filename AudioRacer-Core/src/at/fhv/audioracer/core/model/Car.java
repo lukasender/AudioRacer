lukasender/AudioRacer

commit 57d3fe02dc12e8d7ec6b831184fa21720979219b

Restored 'final BufferedImage'. See '@deprecated' for more information.

AudioRacer-Core/src/at/fhv/audioracer/core/model/Car.java
@@ -19,12 +19,19 @@ public class Car {
 	private Position _position;
 	private Direction _direction;
 	
-	private BufferedImage _image;
+	private final BufferedImage _image;
 	
 	private CarListenerList _listenerList;
 	
+	/**
+	 * @deprecated - there is a reason for why <code>BufferedImage</code> is final! Don't use this constructor anymore. Remove this constructor if you've
+	 *             changed your code (who ever is using it. I don't know).
+	 * @param cardId
+	 */
+	@Deprecated
 	public Car(int cardId) {
 		_carId = cardId;
+		_image = new BufferedImage(0, 0, 0); // this is not useful at all.
 	}
 	
 	public Car(int carId, Position position, Direction direction, BufferedImage image) {

commit e248d2315aab6b4858e900f40b3488e02aa732f6

Fixed bug which I've introduced in 57d3fe0

AudioRacer-Core/src/at/fhv/audioracer/core/model/Car.java
@@ -2,7 +2,9 @@ package at.fhv.audioracer.core.model;
 
 import java.awt.image.BufferedImage;
 
+import at.fhv.audioracer.core.util.Direction;
 import at.fhv.audioracer.core.util.ListenerList;
+import at.fhv.audioracer.core.util.Position;
 
 public class Car {
 	private static class CarListenerList extends ListenerList<ICarListener> implements ICarListener {
@@ -31,7 +33,7 @@ public class Car {
 	@Deprecated
 	public Car(int cardId) {
 		_carId = cardId;
-		_image = new BufferedImage(0, 0, 0); // this is not useful at all.
+		_image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB); // this is not useful at all.
 	}
 	
 	public Car(int carId, Position position, Direction direction, BufferedImage image) {

AudioRacer-Core/src/at/fhv/audioracer/core/model/Direction.java
@@ -1,14 +1,0 @@
-package at.fhv.audioracer.core.model;
-
-public class Direction {
-	private float direction;
-
-	public float getDirection() {
-		return direction;
-	}
-
-	public void setDirection(float direction) {
-		this.direction = direction;
-	}
-	
-}

AudioRacer-Core/src/at/fhv/audioracer/core/util/Direction.java
@@ -1,0 +1,18 @@
+package at.fhv.audioracer.core.util;
+
+public class Direction {
+	private float _direction;
+	
+	public Direction(float direction) {
+		_direction = direction;
+	}
+	
+	public float getDirection() {
+		return _direction;
+	}
+	
+	public void setDirection(float direction) {
+		this._direction = direction;
+	}
+	
+}

AudioRacer-Core/src/at/fhv/audioracer/core/util/Position.java
@@ -1,18 +1,26 @@
-package at.fhv.audioracer.core.model;
+package at.fhv.audioracer.core.util;
 
 public class Position {
 	private float _posX;
 	private float _posY;
 	
+	public Position(float x, float y) {
+		_posX = x;
+		_posY = y;
+	}
+	
 	public float getPosX() {
 		return _posX;
 	}
+	
 	public void setPosX(float posX) {
 		this._posX = posX;
 	}
+	
 	public float getPosY() {
 		return _posY;
 	}
+	
 	public void setPosY(float posY) {
 		this._posY = posY;
 	}

AudioRacer-Server/src/at/fhv/audioracer/server/dao/Car.java
@@ -2,8 +2,8 @@ package at.fhv.audioracer.server.dao;
 
 import java.awt.image.BufferedImage;
 
-import at.fhv.audioracer.core.model.Direction;
-import at.fhv.audioracer.core.model.Position;
+import at.fhv.audioracer.core.util.Direction;
+import at.fhv.audioracer.core.util.Position;
 import at.fhv.audioracer.server.proxy.CarCommunicationProxy;
 
 public class Car extends at.fhv.audioracer.core.model.Car {