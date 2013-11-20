dd78413248e989241aaffe30db254e557a0ad791
Lukas Ender <hello@lukasender.at>
2013-11-20 14:00:23 +0100

    calculated new position

--- AudioRacer-WorldSimulator/src/at/fhv/audioracer/simulator/world/impl/CarClient.java
+++ AudioRacer-WorldSimulator/src/at/fhv/audioracer/simulator/world/impl/CarClient.java
@@ -1,7 +1,5 @@
 package at.fhv.audioracer.simulator.world.impl;
 
-import javax.vecmath.Vector2d;
-
 import at.fhv.audioracer.communication.world.ICarClient;
 import at.fhv.audioracer.core.model.Car;
 import at.fhv.audioracer.core.util.Direction;
@@ -10,6 +8,13 @@ import at.fhv.audioracer.core.util.Position;
 public class CarClient implements ICarClient {
 	
 	private Car _car = null;
+	
+	/**
+	 * 
+	 */
+	private static final float MAX_FULL_LOCK = 4;
+	
+	private static final float SPEED_FACTOR = 1;
 	
 	public void setCar(Car car) {
 		if (car == null) {
@@ -21,33 +26,67 @@ public class CarClient implements ICarClient {
 	@Override
 	public void updateVelocity(float speed, float direction) {
 		// get current position and direction
-		Position pos = _car.getPosition();
-		Direction d = _car.getDirection();
+		Position currentPosition = _car.getPosition();
 		
-		Vector2d v = new Vector2d(pos.getPosX(), pos.getPosY());
+		/*
+		 * Since the car's position is stored as absolute angles (0-360°), we can just add or subtract a relative value.
+		 */
+		Direction newDirection = calcNewDirection(direction, _car.getDirection());
+		Position newPosition = calcNewPosition(speed, currentPosition, newDirection);
 		
-		_car.updatePosition(pos, new Direction(direction));
+		_car.updatePosition(newPosition, newDirection);
 	}
 	
-	public static class V2D extends Vector2d {
-		
-		public V2D(double x, double y) {
-			super(x, y);
-		}
-		
-		/**
-		 * rotate by <code>angle</code>.
-		 * 
-		 * @param angle
-		 *            in radians.
+	/**
+	 * 
+	 * @param direction
+	 *            given direction (-1 <= direction <= 1)
+	 * @param currentDirection
+	 *            current direction in which the car is positioned.
+	 * @return
+	 */
+	private Direction calcNewDirection(float direction, Direction currentDirection) {
+		float deltaAngle = direction * MAX_FULL_LOCK;
+		float newDirection = currentDirection.getDirection() + deltaAngle;
+		return new Direction(newDirection);
+	}
+	
+	private Position calcNewPosition(float speed, Position currentPosition, Direction currentDirection) {
+		// @formatter:off
+		/*
+		             /|
+			     v  / |
+			       /  | b
+		     alpha----+
+			curPos  a
+			  	  
+		 	v = speed * factor
+			alpha = currentDirection in radians
+			
+			sin(alpha) = a/v
+			cos(alpha) = b/v
+			
+			a = v * sin(alpha)
+			b = v * cos(alpha)
+			
+			newPos = pos.x + b, pos.y + a
+			@formatter:on
 		 */
-		public void rotate(double angle) {
-			double x = (this.x * Math.cos(angle)) - (this.y * Math.sin(angle));
-			double y = (this.x * Math.sin(angle)) + (this.y * Math.cos(angle));
-			this.x = x;
-			this.y = y;
-		}
+		// @formatter:on
 		
+		float x = currentPosition.getPosX();
+		float y = currentPosition.getPosY();
+		
+		double alpha = Math.toRadians(currentDirection.getDirection());
+		float v = speed * SPEED_FACTOR;
+		
+		double a = v * Math.sin(alpha);
+		double b = v * Math.cos(alpha);
+		
+		x += (float) b;
+		y += (float) a;
+		
+		return new Position(x, y);
 	}
 	
 }
